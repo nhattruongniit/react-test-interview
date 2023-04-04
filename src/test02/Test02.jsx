29feebaf62761f17c6198b78698f6628cba97431
Tony Nguyen <nhattruong1811@gmail.com>
2023-04-04 20:00:07 +0700

    Update Test02.jsx

--- src/test02/Test02.jsx
+++ src/test02/Test02.jsx
@@ -3,7 +3,11 @@ import React from "react";
 export default function Test02() {
   
   // code something here ...
-
+  
+  React.useEffect(() => {
+    console.log('run effect')
+  },  [])
+  
   return (
     <div className="App">
       <h2>Test02: Skip first render</h2>
